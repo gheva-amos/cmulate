a6640d883c52461b0f260f030f771986881b971e
Amos Gheva <ghevaamos@gmail.com>
2025-08-22 10:39:03 -0400

    Adding the location system

---
+++ include/location_system.h
@@ -0,0 +1,21 @@
+#ifndef CMULATE_LOCATION_SYSTEM_H__
+#define CMULATE_LOCATION_SYSTEM_H__
+#include <memory>
+#include "entity_manager.h"
+#include "world.h"
+
+namespace cmulate
+{
+
+class LocationSystem
+{
+public:
+  void update(std::unique_ptr<EntityManager>& entities,
+      std::unique_ptr<World>& world,
+      EntityManager::Entity entity, float delta_time);
+private:
+};
+
+} // namespace
+
+#endif // CMULATE_LOCATION_SYSTEM_H__
---
+++ src/location_system.cpp
@@ -0,0 +1,21 @@
+#include "location_system.h"
+
+namespace cmulate
+{
+
+void LocationSystem::update(std::unique_ptr<EntityManager>& entities,
+    std::unique_ptr<World>& world,
+    EntityManager::Entity entity, float delta_time)
+{
+  if (entities->positions().count(entity) && entities->positions().count(entity))
+  {
+    entities->location(entity).x() += entities->speed(entity).dx() * delta_time;
+    entities->location(entity).y() += entities->speed(entity).dy() * delta_time;
+    entities->location(entity).z() += entities->speed(entity).dz() * delta_time;
+    world->move_entity(entity, entities->location(entity).x(),
+        entities->location(entity).y());
+  }
+}
+
+} // namespace
+
