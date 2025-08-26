06bac8513e598601248d6613574f9d3ae18a114e
Amos Gheva <ghevaamos@gmail.com>
2025-08-25 23:37:03 -0400

    events are taking care of collisions, not triggers

--- src/systems/physics_system.cpp
+++ src/systems/physics_system.cpp
@@ -4,36 +4,6 @@
 namespace cmulate
 {
 
-class CollisionFunctor : public Functor
-{
-public:
-  virtual void operator()() {}
-  virtual std::string hash_seed() const override { return "Collision functor"; }
-};
-
-class CollisionTrigger : public Trigger
-{
-public:
-  CollisionTrigger(
-    std::unique_ptr<EntityManager>& em,
-    EntityManager::Entity op1,
-    EntityManager::Entity op2) :
-    entities_{em}, op1_{op1}, op2_{op2}
-  {
-    add_function(func_);
-  }
-protected:
-  virtual void do_trigger(Functor& func) override
-  {
-    entities_->handle_collision(op1_, op2_);
-  }
-private:
-  std::unique_ptr<EntityManager>& entities_;
-  EntityManager::Entity op1_;
-  EntityManager::Entity op2_;
-  CollisionFunctor func_;
-};
-
 PhysicsSystem::PhysicsSystem(DataType gravity) :
   gravity_{gravity}
 {
@@ -55,7 +25,7 @@ void PhysicsSystem::handle_collisions(std::unique_ptr<EntityManager>& entities,
 {
   if (entities->collide(entity, other))
   {
-    entities->add_trigger(std::make_unique<CollisionTrigger>(entities, entity, other));
+    entities->handle_collision(entity, other);
   }
 }
 
